5674c4ce6da6810f27b08a8116ed362817532cdc
Bukola <bukolaebikefe@yahoo.com>
2021-12-10 00:01:32 +0100

    added check for nameInput

--- frontend/chat-solidity/src/App.jsx
+++ frontend/chat-solidity/src/App.jsx
@@ -112,8 +112,8 @@ function App() {
 
   return (
     <div className="App bg-bgWhite h-screen md:overflow-y-hidden overflow-y-scroll styleScroll">
-      {/* <div className="flex justify-center items-center pt-48 md:pt-24 flex-col">
-        <Profile style={"md:w-56 md:h-56 w-32 h-32"}/>
+      <div className="flex justify-center items-center pt-48 md:pt-24 flex-col">
+        <Profile style={"md:w-56 md:h-56 w-32 h-32"} />
         {!currentAccount &&
           (loading ? null : (
             <Button
@@ -133,13 +133,13 @@ function App() {
             Please connect to Metamask, If you don't have, you can get it here
           </p>
         )}
-        {currentAccount && <Nameinput />} */}
-      {/* <button onClick={wave}>Wave</button> */}
-      {/* {!currentAccount && (
+        {currentAccount && <Nameinput />}
+        {/* <button onClick={wave}>Wave</button> */}
+        {/* {!currentAccount && (
             <button onClick={connectWallet}>Connect Account</button>
           )} */}
-      {/* </div> */}
-      <ChatPage />
+      </div>
+      {/* <ChatPage /> */}
     </div>
   );
 }
--- frontend/chat-solidity/src/components/Nameinput.jsx
+++ frontend/chat-solidity/src/components/Nameinput.jsx
@@ -3,41 +3,53 @@ import ErrorMessage from "./ErrorMessage";
 
 const Nameinput = ({}) => {
   const [value, setValue] = useState("");
-  const [emptyInput, setEmptyInput] = useState(false);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (value.length === 0) {
-      return setEmptyInput(true);
-    }
-    console.log(value);
-  };
+  const [emptyInput, setEmptyInput] = useState(true);
+  const [longName, setLongName] = useState(false);
 
   const handleChange = (e) => {
     setValue(e.target.value);
-    setEmptyInput(false);
+    if (e.target.value.length > 0) {
+      setEmptyInput(false);
+      if (e.target.value.length >= 15) {
+        setLongName(true);
+      } else {
+        setLongName(false);
+      }
+    } else {
+      setEmptyInput(true);
+    }
+    // value.length > 0 && setEmptyInput(false);
   };
 
   return (
-    <div className="w-screen px-10 md:w-96 mt-8 ">
-      <form className="flex flex-col" onSubmit={(e) => handleSubmit(e)}>
+    <div className="w-full px-10 md:w-96 mt-8 ">
+      <form className="flex flex-col">
         <input
           type="text"
           value={value}
           onChange={(e) => handleChange(e)}
           placeholder="What should we call you?"
           className={`w-full p-2 rounded-md outline-none text-textBlue font-semibold ${
-            emptyInput && "ring-1 ring-red-300"
-          } ${value.length > 0 && "ring-1 ring-blue-300"}`}
+            value.length > 0 && "ring-1 ring-blue-300"
+          }`}
+          maxLength={15}
         />
-        {emptyInput && <ErrorMessage text="Don't be shy, put in a name :)" />}
-        <button
+        {longName && <ErrorMessage text="Make it a shorter name :/" />}
+        {!emptyInput && (
+          <p
+            role="button"
+            className={`mt-4 w-fulll text-center bg-boldGreen py-2 px-3 rounded-md bg-opacity-60 hover:bg-opacity-70 ease-in text-textBlue font-semibold shadow-md`}
+          >
+            Submit
+          </p>
+        )}
+        {/* <button
           type={"submit"}
           onClick={(e) => handleSubmit(e)}
           className={`mt-4 w-full bg-boldGreen py-2 px-3 rounded-md bg-opacity-60 hover:bg-opacity-70 ease-in text-textBlue font-semibold shadow-md`}
         >
           Submit
-        </button>
+        </button> */}
       </form>
     </div>
   );
